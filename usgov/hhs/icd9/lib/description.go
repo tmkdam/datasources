8dfd7111bd449e14fba073ddb507fd91273de01d
Michael M. Wasser <untoldone@users.noreply.github.com>
2015-07-22 10:53:10 -0700

    adding icd9

--- usgov/hhs/icd9/lib/description.go
+++ usgov/hhs/icd9/lib/description.go
@@ -1,16 +1,15 @@
 package lib
 
 import (
-  "io"
-  "io/ioutil"
-  "regexp"
-  "bytes"
-  "archive/zip"
+  "os"
+  "encoding/csv"
   "bitbucket.org/gocodo/bloomsource"
   "bitbucket.org/gocodo/bloomsource/helpers"
 )
 
 type Description struct {}
+
+var url = "https://s3.amazonaws.com/gocodo/usgov/hhs/icd/icd-9-cm.csv"
 
 func (d *Description) Available() ([]bloomsource.Source, error) {
   return []bloomsource.Source{
@@ -22,68 +21,44 @@ func (d *Description) Available() ([]bloomsource.Source, error) {
 }
 
 func (d *Description) FieldNames(sourceName string) ([]string, error) {
-  reader, err := getFileReader("http://www.cms.gov/Medicare/Coding/ICD9ProviderDiagnosticCodes/Downloads/ICD-9-CM-v32-master-descriptions.zip", regexp.MustCompile(`CMS32_DESC_LONG_SHORT_DX.xlsx$`))
-  if err != nil {
-    return nil, err
-  }
-
-  allBytes, err := ioutil.ReadAll(reader)
-  if err != nil {
-  	return nil, err
-  }
-
-  bReader := bytes.NewReader(allBytes)
-
-  zipReader, err := zip.NewReader(bReader, int64(bReader.Len()))
-  if err != nil {
-  	return nil, err
-  }
-
-  xlReader, err := helpers.NewXlxsReader(zipReader, "Sheet1")
-  if err != nil {
-  	return nil, err
-  }
-
-	return xlReader.Headers(), nil
-}
-
-func getFileReader(uri string, zipPattern *regexp.Regexp) (io.Reader, error) {
   downloader := bloomsource.NewDownloader("data/", nil)
-  path, err := downloader.Fetch(uri)
+  path, err := downloader.Fetch(url)
   if err != nil {
     return nil, err
   }
 
-  reader, err := helpers.OpenExtractZipReader(path, zipPattern)
+  fileReader, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer fileReader.Close()
+
+  csvReader := csv.NewReader(fileReader)
   if err != nil {
     return nil, err
   }
 
-  return reader, nil
+  columns, err := csvReader.Read()
+  if err != nil {
+    return nil, err
+  }
+
+  return columns, nil
 }
 
 func (d *Description) Reader(source bloomsource.Source) (bloomsource.ValueReader, error) {
-  reader, err := getFileReader("http://www.cms.gov/Medicare/Coding/ICD9ProviderDiagnosticCodes/Downloads/ICD-9-CM-v32-master-descriptions.zip", regexp.MustCompile(`CMS32_DESC_LONG_SHORT_DX.xlsx$`))
+  downloader := bloomsource.NewDownloader("data/", nil)
+  path, err := downloader.Fetch(url)
   if err != nil {
     return nil, err
   }
 
-  allBytes, err := ioutil.ReadAll(reader)
+  fileReader, err := os.Open(path)
   if err != nil {
-  	return nil, err
+    return nil, err
   }
 
-  bReader := bytes.NewReader(allBytes)
+  csvReader := helpers.NewCsvReader(fileReader)
 
-  zipReader, err := zip.NewReader(bReader, int64(bReader.Len()))
-  if err != nil {
-  	return nil, err
-  }
-
-  xlReader, err := helpers.NewXlxsReader(zipReader, "Sheet1")
-  if err != nil {
-  	return nil, err
-  }
-
-  return xlReader, nil
+  return csvReader, nil
 }
