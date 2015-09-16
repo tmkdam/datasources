6b1ba02af92fcf1e09afc1743492ea92daf285a1
Michael M. Wasser <untoldone@users.noreply.github.com>
2015-09-15 17:33:21 -0700

    adding 2014 aco perf

--- usgov/hhs/aco/lib/description.go
+++ usgov/hhs/aco/lib/description.go
@@ -13,6 +13,7 @@ var sourceUris = map[string]string {
   "usgov.hhs.aco_contacts": "https://s3.amazonaws.com/gocodo/usgov/hhs/aco/aco_contacts.csv",
   "usgov.hhs.aco_puf": "https://s3.amazonaws.com/gocodo/usgov/hhs/aco/aco_puf.csv",
   "usgov.hhs.aco_year1": "https://s3.amazonaws.com/gocodo/usgov/hhs/aco/aco_year1.csv",
+  "usgov.hhs.aco_performance_2014": "https://s3.amazonaws.com/gocodo/usgov/hhs/aco/aco_performance_2014.csv",
   "usgov.hhs.aco": "https://s3.amazonaws.com/gocodo/usgov/hhs/aco/aco_names.csv",
   "usgov.hhs.aco_benchmarks": "https://s3.amazonaws.com/gocodo/usgov/hhs/aco/aco_benchmarks.csv",
   "usgov.hhs.aco_measures": "https://s3.amazonaws.com/gocodo/usgov/hhs/aco/aco_measures.csv",
@@ -27,6 +28,10 @@ func (d *Description) Available() ([]bloomsource.Source, error) {
       Version: "2015-00",
     },
     bloomsource.Source{
+      Name: "usgov.hhs.aco_performance_2014",
+      Version: "2015-00",
+    },
+    bloomsource.Source{
       Name: "usgov.hhs.aco_puf",
       Version: "2015-00",
     },
@@ -36,7 +41,7 @@ func (d *Description) Available() ([]bloomsource.Source, error) {
     },
     bloomsource.Source{
       Name: "usgov.hhs.aco",
-      Version: "2015-00",
+      Version: "2015-01",
     },
     bloomsource.Source{
       Name: "usgov.hhs.aco_benchmarks",
